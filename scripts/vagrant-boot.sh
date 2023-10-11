53fb47c55af9fcb54ac7b7e4d9b9e57c9bcf512f
Mahbub Alam <alam.mahbub214@gmail.com>
2023-10-11 13:32:04 -0400

    Install llvm tool set

--- scripts/vagrant-boot.sh
+++ scripts/vagrant-boot.sh
@@ -8,3 +8,5 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 
 sudo apt-get install -y python3.10 python3.10-dev python3.10-venv
 sudo ln -s /usr/bin/python3.10 /usr/bin/python
+
+sudo apt install clang lldb lld
